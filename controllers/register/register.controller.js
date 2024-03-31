1210fdacaa8f092f6d2de6c14d2120072bce96d6
Javad Yousefi <javadyousefi9000@gmail.com>
2024-03-31 18:52:15 +0330

    check validtion of data

--- controllers/register/register.controller.js
+++ controllers/register/register.controller.js
@@ -23,6 +23,27 @@ async function register(req, res) {
                 return; // End the function execution here
             }
 
+            // Validations
+            const { firstname, lastname, email, password } = parsedJson;
+            const trimmedFirstName = String(firstname).trim();
+            const trimmedLastName = String(lastname).trim();
+            const trimmedEmail = String(email).trim();
+            const trimmedPassword = String(password).trim();
+
+            if (
+                !(trimmedFirstName.length >= 3 &&
+                    trimmedLastName.length >= 3 &&
+                    /^\S+@\S+\.\S+$/.test(trimmedEmail) &&
+                    trimmedPassword.length >= 6)
+            ) {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({
+                    message: 'Invalid data format!',
+                }));
+                return; // End the function execution here
+            }
+
+
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({
                 message: 'it is ok',
