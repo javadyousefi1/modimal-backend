7a758a6ab2291489e93342fec9283c75c9a070e1
Javad Yousefi <javadyousefi9000@gmail.com>
2024-03-31 18:12:37 +0330

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        data: 'Hello World!',
+    }));
+})
+
+server.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
