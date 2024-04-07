203ff5c4395b52e6e9bdf97b473de6637cfcf5d1
javad yosuefi <javadyousefi9000@gmail.com>
2024-04-07 13:00:52 -0900

    fix bug

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3000
 
 const server = http.createServer((req, res) => {
     // Set CORS headers
-    res.setHeader('Access-Control-Allow-Origin', 'https://javadyousefi.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://javadyousefi.com/*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
