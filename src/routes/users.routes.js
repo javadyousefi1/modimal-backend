ecd453378052da74b353742ac328d95c263cc577
Javad Yousefi <javadyousefi9000@gmail.com>
2024-05-09 18:28:36 +0330

    add logout

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -14,7 +14,15 @@ const { getUserController } = require("../controller/users.controller");
  *     responses:
  *       200:
  *         description: API is running
- */ 
+ */
 router.get("/", getUserController);
+router.get("/logout", (req, res) => {
+  // Clear the JWT from the cookie
+  res.clearCookie("token");
+  // Send a success message
+  res
+    .status(200)
+    .json({ statusCode: res.statusCode, message: "Successfully logged out" });
+});
 
 module.exports = router;
